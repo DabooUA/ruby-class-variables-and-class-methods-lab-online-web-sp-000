74679e17e6c6f752f095bef6a171b27ec479e295
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-03 20:18:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+
+  attr_reader :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.unique!
+  end
+
+  def self.artists
+    @@artists.unique!
+  end
+
+
+end
