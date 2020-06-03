2eb1fd37d058ade2f2303b5b5ede8d9d5802ddff
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-03 20:34:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,5 +27,13 @@ class Song
     @@artists.uniq!
   end
 
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
+  def self.artists_count
+    artist_count = Hash.new(0)
 
 end
